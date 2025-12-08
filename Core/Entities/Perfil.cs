92268b3f5e95a738907f83e8e9a2088522696af1
renan <renan.massini@tegma.com.br>
2025-12-08 18:41:34 -0300

    Mudança de estrutura

--- Core/Entities/Perfil.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Entities;
-
-public class Perfil
-{
-    public int Id { get; set; }
-    public string Nome { get; set; } = null!;
-
-    public List<UsuarioPerfil> UsuarioPerfis { get; set; } = new();
-}
