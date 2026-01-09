497f4e28b3f60fc6187e905a282a18167ad4add4
renan <renan.massini@tegma.com.br>
2026-01-09 20:18:56 -0300

    commit para atualização

--- API/AgendaBelezuza.Api/AgendaBelezuza.Application/obj/Debug/net8.0/AgendaBelezuza.Application.AssemblyInfo.cs
+++ API/AgendaBelezuza.Api/AgendaBelezuza.Application/obj/Debug/net8.0/AgendaBelezuza.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaBelezuza.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbaded0dab0e8a17d644820cd91dea7ab1926383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44e6e1a0406f463c8fe8b38c5c0df38d4a445c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaBelezuza.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaBelezuza.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- API/AgendaBelezuza.Api/AgendaBelezuza.Infrastructure/obj/Debug/net8.0/AgendaBelezuza.Infrastructure.AssemblyInfo.cs
+++ API/AgendaBelezuza.Api/AgendaBelezuza.Infrastructure/obj/Debug/net8.0/AgendaBelezuza.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaBelezuza.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e5759761ca8d24e74e8d71a084c64d1b831c2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44e6e1a0406f463c8fe8b38c5c0df38d4a445c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaBelezuza.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaBelezuza.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
